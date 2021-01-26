39e97e8ed68a7f8942e88086be70907cab14d70e
shengli <rw2423@nyu.edu>
2021-01-26 20:55:03 +0800

    minor styling edits

--- coding-exercises/placeholder/coding-foundation/script.js
+++ coding-exercises/placeholder/coding-foundation/script.js
@@ -1,13 +1,12 @@
 function generate(){
     var numSpaces = document.getElementById("numBoxVal").value;
     var i;
-        for(i = 0; i < numSpaces; i++){
-
-            var node = document.createElement("p");
-            var textnode = document.createTextNode("hey there");
-            node.appendChild(textnode);
-            document.getElementById("bodyDiv").appendChild(node);       
-}
+        for(i = 0; i < numSpaces; i++){ 
+            var node = document.createElement("p"); //makes a p element for the emoji
+            var textnode = document.createTextNode("🙃"); //specifies the text in the node, in this case an emoji
+            node.appendChild(textnode); //append textnode to the element which is called node
+            document.getElementById("emojiP").appendChild(node);
+    }
 }
 
 
