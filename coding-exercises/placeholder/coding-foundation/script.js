2d20ccb1b56fd03f089ca160fc6a50cb7cc2ef52
shengli <rw2423@nyu.edu>
2021-01-26 19:15:41 +0800

    each click generates an additional hey there

--- coding-exercises/placeholder/coding-foundation/script.js
+++ coding-exercises/placeholder/coding-foundation/script.js
@@ -1,4 +1,21 @@
-document.getElementById("genBtn").addEventListener("click", function
- drawSquares(){
-    document.getElementById("bodydiv").innerHTML = "hey there";
-});
+var numSpaces = document.getElementById("numBoxVal");
+
+var i;
+for(i = 0; i < numSpaces; i++){
+    
+}
+
+function generate(){
+var node = document.createElement("p");
+var textnode = document.createTextNode("hey there");
+node.appendChild(textnode);
+
+document.getElementById("bodyDiv").appendChild(node);
+}
+
+
+
+// document.getElementById("genBtn").addEventListener("click", function
+//  drawSquares(){
+//     document.getElementById("bodyDiv").innerHTML = "hey there";
+// });
