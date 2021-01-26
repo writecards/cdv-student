9fd2c5d38bb54726c1028154c44a8811d6a9b4ed
shengli <rw2423@nyu.edu>
2021-01-26 20:02:17 +0800

    added for loop

--- coding-exercises/placeholder/coding-foundation/script.js
+++ coding-exercises/placeholder/coding-foundation/script.js
@@ -1,21 +1,14 @@
-var numSpaces = document.getElementById("numBoxVal");
-
-var i;
-for(i = 0; i < numSpaces; i++){
-    
-}
-
 function generate(){
-var node = document.createElement("p");
-var textnode = document.createTextNode("hey there");
-node.appendChild(textnode);
+    var numSpaces = document.getElementById("numBoxVal").value;
+    var i;
+        for(i = 0; i < numSpaces; i++){
 
-document.getElementById("bodyDiv").appendChild(node);
+            var node = document.createElement("p");
+            var textnode = document.createTextNode("hey there");
+            node.appendChild(textnode);
+            document.getElementById("bodyDiv").appendChild(node);       
+}
 }
 
 
 
-// document.getElementById("genBtn").addEventListener("click", function
-//  drawSquares(){
-//     document.getElementById("bodyDiv").innerHTML = "hey there";
-// });
