96c4ab96241ada05138febb10d7a7df44dcb0370
shengli <rw2423@nyu.edu>
2021-02-02 22:17:45 +0800

    lab 1

---
+++ coding-exercises/placeholder/lab1/script.js
@@ -0,0 +1,413 @@
+let data = [
+    {
+        "timestamp": "2021-01-28T04:10:59.346Z",
+        "brain beanie🧠": 4,
+        "micro bag👜": 5,
+        "croc gloves🧤": 2,
+        "extendo pants👖": 4,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T04:16:59.223Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 4,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 3
+    },
+    {
+        "timestamp": "2021-01-28T04:19:53.063Z",
+        "brain beanie🧠": 4,
+        "micro bag👜": 3,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 4
+    },
+    {
+        "timestamp": "2021-01-28T04:21:33.555Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 5,
+        "croc gloves🧤": 2,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T04:33:26.658Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 4,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T04:45:26.649Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 3,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T04:48:54.554Z",
+        "brain beanie🧠": 5,
+        "micro bag👜": 1,
+        "croc gloves🧤": 3,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T05:45:18.446Z",
+        "brain beanie🧠": 4,
+        "micro bag👜": 3,
+        "croc gloves🧤": 3,
+        "extendo pants👖": 4,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T05:52:48.493Z",
+        "brain beanie🧠": 4,
+        "micro bag👜": 3,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 4
+    },
+    {
+        "timestamp": "2021-01-28T05:57:17.098Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 3,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T06:45:22.424Z",
+        "brain beanie🧠": 4,
+        "micro bag👜": 3,
+        "croc gloves🧤": 3,
+        "extendo pants👖": 4,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T08:45:10.503Z",
+        "brain beanie": 5,
+        "micro bag": 2,
+        "crocGloves": 1,
+        "extendoPants": 1,
+        "bananciaga": 4
+    },
+    {
+        "timestamp": "2021-01-28T08:45:30.178Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 2,
+        "croc gloves🧤": 5,
+        "extendo pants👖": 5,
+        "bananciaga🍌": 5
+    },
+    {
+        "timestamp": "2021-01-28T08:46:47.772Z",
+        "brain beanie🧠": 5,
+        "micro bag👜": 2,
+        "croc gloves🧤": 5,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T08:52:41.508Z",
+        "brain beanie🧠": 2,
+        "micro bag👜": 3,
+        "croc gloves🧤": 2,
+        "extendo pants👖": 4,
+        "bananciaga🍌": 3
+    },
+    {
+        "timestamp": "2021-01-28T10:30:17.879Z",
+        "brain beanie🧠": 5,
+        "micro bag👜": 5,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T10:45:18.384Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 4,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T11:02:23.648Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 1,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T11:24:43.183Z",
+        "brain beanie🧠": 5,
+        "micro bag👜": 1,
+        "croc gloves🧤": 4,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-28T14:09:31.494Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 4,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T15:58:11.552Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 3,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-28T19:40:54.059Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 1,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-01-29T15:30:31.105Z",
+        "brain beanie🧠": 3,
+        "micro bag👜": 1,
+        "croc gloves🧤": 3,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-30T04:35:58.139Z",
+        "brain beanie🧠": 3,
+        "micro bag👜": 2,
+        "croc gloves🧤": 1,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 2
+    },
+    {
+        "timestamp": "2021-01-31T08:36:39.852Z",
+        "brain beanie🧠": 2,
+        "micro bag👜": 3,
+        "croc gloves🧤": 2,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 4
+    },
+    {
+        "timestamp": "2021-01-31T09:42:40.195Z",
+        "brain beanie": 2,
+        "micro bag": 3,
+        "crocGloves": 2,
+        "extendoPants": 1,
+        "bananciaga": 5
+    },
+    {
+        "timestamp": "2021-01-31T12:05:35.290Z",
+        "brain beanie": 1,
+        "micro bag": 4,
+        "crocGloves": 4,
+        "extendoPants": 1,
+        "bananciaga": 1
+    },
+    {
+        "timestamp": "2021-01-31T14:19:39.755Z",
+        "brain beanie🧠": 4,
+        "micro bag👜": 5,
+        "croc gloves🧤": 3,
+        "extendo pants👖": 2,
+        "bananciaga🍌": 4
+    },
+    {
+        "timestamp": "2021-01-31T17:09:31.650Z",
+        "brain beanie🧠": 1,
+        "micro bag👜": 1,
+        "croc gloves🧤": 5,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 3
+    },
+    {
+        "timestamp": "2021-02-01T01:41:13.689Z",
+        "brain beanie🧠": 2,
+        "micro bag👜": 4,
+        "croc gloves🧤": 2,
+        "extendo pants👖": 1,
+        "bananciaga🍌": 1
+    },
+    {
+        "timestamp": "2021-02-01T01:41:48.551Z",
+        "brain beanie🧠": 5,
+        "micro bag👜": 2,
+        "croc gloves🧤": 5,
+        "extendo pants👖": 5,
+        "bananciaga🍌": 2
+    }
+]
+
+//function copied from leon 
+function averageData(data){
+    let newData = [];
+    let keys = Object.keys(data[data.length-1]);
+    for(let i = 0; i < keys.length; i++){
+        let key = keys[i];
+        let sum = 0;
+        let num = 0;
+        for(let j = 0; j < data.length; j++){
+            let datum = data[j];
+            if(key in datum){ //what does in mean
+                sum += datum[key];
+                num++
+            }
+        }
+        let avg = sum/num;
+        if(!isNaN(avg)){
+            let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
+            newData.push(newDataPoint);
+        }
+    }
+    return newData;
+}
+
+//using the function
+let transformedData = averageData(data);
+
+console.log(transformedData);
+let container = document.getElementById("container");
+
+for(let i = 0; i < transformedData.length; i++){
+    let datapoint = transformedData[i];
+    let fashionTrend = datapoint.name;
+    let average = datapoint.average;
+    let bar = document.createElement("div");
+
+    bar.className = "bar";
+    
+    let barName = document.createElement("p");
+    barName.className = "barName";
+  //  barName.innerHTML = fashionTrend;
+
+    let percentageScore = Math.floor(mapRange(average, 0, 5, 0, 100));
+    
+    let percentageString = percentageScore.toString();
+    
+    
+    bar.appendChild(barName);
+    container.appendChild(bar);
+    let barHeight = bar.style.height;
+    
+
+
+    let startWidth = 30;
+    let startHeight = 30;
+    bar.style.height = startHeight + "px";
+    bar.style.width = startWidth + "px";
+    
+    
+
+    let barWidth = Math.floor(mapRange(average,1,5,0,600)); 
+    bar.addEventListener('mouseover',function(){
+        bar.style.width = barWidth + "px";  
+        barName.style.display = "block"; 
+       
+    });
+    bar.addEventListener('mouseout',function(){
+        bar.style.width = startWidth + "px";
+        barName.style.display = "none";
+        barName.style.display.transitionDelay = secondsDelay + "s";
+    
+    });
+   barName.style.display = "none";
+//changing the color for each individual bar
+let secondsDelay = 2;
+
+    if(i == 0){
+        bar.style.backgroundColor = "lightpink";
+        bar.addEventListener('mouseover',function(){
+            barName.innerHTML = "🧠" + percentageString + "%";
+            bar.style.backgroundColor = "lightcoral";
+            bar.style.transitionDelay = 0 + "s";
+        });  
+        bar.addEventListener('mouseout',function(){
+            bar.style.width = startWidth + "px";
+            bar.style.backgroundColor = "lightpink";
+            bar.style.transitionDelay = secondsDelay + "s";
+            
+        });
+       
+    }else if (i == 1){
+        barName.innerHTML = "👜" + percentageString + "%";
+        bar.style.backgroundColor = "peachpuff";
+        bar.addEventListener('mouseover',function(){
+            bar.style.backgroundColor = "orange";
+            bar.style.transitionDelay = 0 + "s";
+
+        });   
+        bar.addEventListener('mouseout',function(){
+            bar.style.width =startWidth + "px";
+            bar.style.backgroundColor = "peachpuff";
+            bar.style.transitionDelay = secondsDelay + "s";
+        });
+         
+  
+    }else if (i == 2){
+        barName.innerHTML = "🧤" + percentageString + "%";
+        bar.style.backgroundColor = "lightskyblue";
+        bar.addEventListener('mouseover',function(){
+            bar.style.backgroundColor = "deepskyblue";
+            bar.style.transitionDelay = 0 + "s";
+        });  
+        bar.addEventListener('mouseout',function(){
+            bar.style.width =startWidth + "px";
+            bar.style.backgroundColor = "lightskyblue";
+            bar.style.transitionDelay = secondsDelay + "s";
+        }); 
+    }else if (i == 3){
+        barName.innerHTML = "👖" + percentageString + "%";
+        bar.style.backgroundColor = "cornflowerblue";  
+        bar.addEventListener('mouseover',function(){
+            bar.style.backgroundColor = "dodgerblue"; 
+            bar.style.transitionDelay = 0 + "s";
+        }); 
+        bar.addEventListener('mouseout',function(){
+            bar.style.width = startWidth + "px";
+            bar.style.backgroundColor = "cornflowerblue"; 
+            bar.style.transitionDelay = secondsDelay + "s";
+        });  
+    }else if (i == 4){
+        barName.innerHTML = "🍌" + percentageString + "%";
+        bar.style.backgroundColor = "khaki"; 
+        bar.addEventListener('mouseover',function(){
+            bar.style.backgroundColor = "gold"; 
+            bar.style.transitionDelay = 0 + "s";
+           
+        });  
+        bar.addEventListener('mouseout',function(){
+            bar.style.width = startWidth + "px";
+            bar.style.backgroundColor = "khaki"; 
+            bar.style.transitionDelay = secondsDelay + "s";
+        }); 
+    }
+   
+   
+    
+
+}
+//recreates map() from p5.js, help from stackoverflow
+function mapRange(value,a,b,c,d){
+    value = (value-a)/(b-a);
+    return c + value * (d-c);
+}
+
+
+
+
+
+
+
+
+  
